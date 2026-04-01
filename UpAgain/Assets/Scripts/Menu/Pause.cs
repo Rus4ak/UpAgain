a2798d6392a13303a4bb71b181e1d2ec4baab315
Dmytro <rus4ak331@gmail.com>
2026-04-01 22:16:27 +0300

    Fix the stone sound

--- UpAgain/Assets/Scripts/Menu/Pause.cs
+++ UpAgain/Assets/Scripts/Menu/Pause.cs
@@ -1,13 +1,21 @@
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 
 public class Pause : MonoBehaviour
 {
+    [SerializeField] private AudioMixer _mixer;
+
     public void SetTimeScale(int number)
     {
         Time.timeScale = number;
 
         GameManager.Instance.playerMovement.SetMove(number == 1);
+
+        if (number == 0)
+            _mixer.SetFloat("Audio", -80f);
+        else
+            _mixer.SetFloat("Audio", 0f);
     }
 
     public void LoadMainMenu()
